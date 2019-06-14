f61cc8aa6c0c618901d258ce3ed08605f162dda9
The Natural <philip.smalls86@gmail.com>
2019-06-14 20:30:06 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,7 +16,7 @@ def get_contestant_name(data, occupation)
     info.each do |person|
       person.each do |key, val|
         if val == occupation
-          return person
+          return person['name']
      binding.pry 
         end
       end
