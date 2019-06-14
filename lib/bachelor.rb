e46a3476328cb0e7fbcff6712425219a9176c7f9
The Natural <philip.smalls86@gmail.com>
2019-06-14 21:05:50 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,64 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+   contestant.each do |key, val|
+     if val == "Winner"
+       return contestant["name"].split(' ')[0]
+      end
+    end
+  end
 end
 
+
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, info|
+    info.each do |person|
+      person.each do |key, val|
+        if val == occupation
+          return person['name']
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+    data.each do |season, info|
+      info.each do |person|
+        person.each do |key, val|
+          if val == hometown
+            counter += 1
+          end
+        end
+      end
+    end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, info|
+    info.each do|person|
+      person.each do |key, val|
+        if val == hometown
+          return person['occupation']
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  counter = 0
+  ages = 0
+    data[season].each do |person|
+      person.each do |key, val|
+        if key == 'age'
+          ages += val.to_f
+          counter += 1
+        end
+      end
+    end
+    return (ages/counter).round
 end
