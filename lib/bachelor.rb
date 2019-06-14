b7fbb0e987afee4487ab8c5a415cc47a3674e670
The Natural <philip.smalls86@gmail.com>
2019-06-14 20:58:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -54,7 +54,7 @@ def get_average_age_for_season(data, season)
     data[season].each do |person|
       person.each do |key, val|
         if key == age
-          return val.to_i
+          counter += val.to_i
       binding.pry
       end
     end
