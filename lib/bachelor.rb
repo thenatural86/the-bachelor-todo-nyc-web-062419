f980373a0beef8060734928dcd33dbc1a63db678
The Natural <philip.smalls86@gmail.com>
2019-06-14 20:00:17 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ require 'pry'
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
      binding.pry 
-   if contestant == "winner"
+   if contestant[:status] == "winner"
     end
   end
 end
